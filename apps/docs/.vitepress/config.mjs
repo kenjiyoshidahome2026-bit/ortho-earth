6e54d7602970705487c22898870d46671a7ed7de
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-05-07 15:06:01 +0900

    docs

--- apps/docs/.vitepress/config.mjs
+++ apps/docs/.vitepress/config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: 'https://www.ortho-earth.com/' },
+      { text: 'www', link: 'https://www.ortho-earth.com/' },
       { text: 'Examples', link: '/markdown-examples' }
     ],
 
