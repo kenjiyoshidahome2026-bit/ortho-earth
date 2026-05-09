acceb382bf9284951ed08347bec431530bd2f595
Kenji Yoshida <kenji.yoshida.home.2026@gmail.com>
2026-05-09 16:36:25 +0900

    cleanup

--- packages/ortho-map/src/workers/border.js
+++ packages/ortho-map/src/workers/border.js
@@ -2,29 +2,23 @@ import { geoPath, geoOrthographic, geoGraticule10 } from 'd3-geo';
 import { comma, dateArray, timeArray, L2, isObject } from "common";
 import { geopbf } from "geopbf";
 
-let canvas, layer, width, height, dpr, path, zoom, attribution;
 const accessories = {
 	borders:{minZoom:2, maxZoom:7, graticule:true, border:true, maritime:true, geolines:true },
 	globe:{maxZoom:8, size:125, bottom:30, right:20, land:"rgb(160,200,160)",sea:"rgb(200,240,255)",line:"rgb(150,0,0)"},
-	stars:{maxZoom:3, maxCount:Infinity},
+	stars:{maxZoom:2, maxCount:Infinity},
 	night:{maxZoom:2},
 	latlng:{minZoom:2, color:"white"},
 	scale:{minZoom:2, color:"white", unit: "metric"},
 	credit:{minZoom:2, color:"white"},
 };
-
 const { sin, cos, asin, hypot, atan2, log2, log10, floor, PI } = Math, rad = PI / 180;
 const getSidereal = d => ((18.697374 + 24.0657098 * ((d.getTime() + d.getTimezoneOffset() * 60000) / 864e5 + 2440587.5 - 2451545.0)) * 15) % 360;
+let canvas, layer, width, height, dpr, path, zoom, attribution;
+let active = false, noCircle = 3, isNarrow = false;
 
 const proj = geoOrthographic();
-let active = false;
-let noCircle = 1;
-let isNarrow = false;
 const sphere = { type: "Sphere" };
 const graticule = geoGraticule10();
-let land110, stars;
-
-
 
 const funcs = { init, set, drawing, drawn, move, leave, resize, destroy };
 onmessage = e => funcs[e.data.type](e.data);
@@ -43,7 +37,6 @@ async function set(data) { if (data.data != "options") return postMessage({ type
 			for (let j in accessories[i]) accessories[i][j] = opts[i][j];
 		}
 	}
-	console.log(accessories)
 	accessories.latlng.names = [
 		{ en: "LAT", ja: "緯度", zh: "纬度", ko: "위도" }[lang],
 		{ en: "LNG", ja: "経度", zh: "经度", ko: "경도" }[lang],
@@ -54,22 +47,23 @@ async function set(data) { if (data.data != "options") return postMessage({ type
 		"ne_50m_admin_0_boundary_lines_maritime_indicator",
 		"ne_50m_geographic_lines",
 		"ne_110m_land", "stars.6"].map(geopbf))).map(t=>t.geojson);
-	if (accessories.borders) {
-		const borders = accessories.borders.jsons = [[sphere, { stroke: "rgba(200,200,200,0.8)", width: 0.8 }]];
-		accessories.borders.graticule === false || borders.push([graticule, { stroke: "rgba(255, 255, 255, 0.5)", width: 0.5 }]);
-		accessories.borders.border === false || borders.push([jsons[0], { stroke: "rgba(255,255,255,0.8)", width: 1, dash: [3, 1] }]);
-		accessories.borders.maritime === false || borders.push([jsons[1], { stroke: "rgba(128,128,255,0.8)", width: 0.8, dash: [3, 1] }]);
-		accessories.borders.geolines === false || borders.push([jsons[2], { stroke: "rgba(255,255,255,1)", width: 0.5, dash: [4, 2] }]);
+	const q = accessories.borders;
+	if (q) {
+		const borders = q.jsons = [[sphere, { stroke: "rgba(200,200,200,0.8)", width: 0.8 }]];
+		q.graticule === false || borders.push([graticule, { stroke: "rgba(255, 255, 255, 0.5)", width: 0.5 }]);
+		q.border === false || borders.push([jsons[0], { stroke: "rgba(255,255,255,0.8)", width: 1, dash: [3, 1] }]);
+		q.maritime === false || borders.push([jsons[1], { stroke: "rgba(128,128,255,0.8)", width: 0.8, dash: [3, 1] }]);
+		q.geolines === false || borders.push([jsons[2], { stroke: "rgba(255,255,255,1)", width: 0.5, dash: [4, 2] }]);
 	}
-	land110 = jsons[3];
-	stars = jsons[4].features.map(f => {
+	accessories.globe.json = jsons[3];
+	accessories.stars.data = jsons[4].features.map(f => {
 		const c = f.geometry.coordinates, p = f.properties;
 		const bv = (v => v < -0.3 ? "#b2c8ff" : v < 0.0 ? "#d9e2ff" : v < 0.3 ? "#f8faff" : v < 0.6 ? "#fff8f0" :
 			v < 0.8 ? "#fff2c8" : v < 1.1 ? "#ffe0b5" : v < 1.4 ? "#ffcc99" : "#ffab91")(p.bv);
 		return { x: c[0] * rad, y: c[1] * rad, mag: p.mag, bv };
 	}).sort((p,q)=>p.mag>q.mag?1:-1).slice(0,accessories.stars.maxCount);
 	active = true;
-	setInterval(()=>{accessories.night.json = nightJSON(new Date()), draw_night(); draw_stars()},1000);
+	setInterval(()=>{accessories.night.json = nightJSON(new Date()), draw()},1000);
 	postMessage({ type: data.type, action: "done" });
 }
 function resize(data) {
@@ -96,8 +90,8 @@ function drawing(data) {
 		draw_credit();
 	});
 }
-function move(data) { const q = accessories.latlng.names;
-	accessories.latlng.string = data.lat?`${q[0]}: ${data.lat.toFixed(6)} ${q[1]}: ${data.lng.toFixed(6)}${data.alt ? ` ${q[2]}: ${data.alt.toFixed(1)}[m]` : ""}`:"";
+function move(data) { const q = accessories.latlng, names = q.names;
+	q.string = data.lat?`${names[0]}: ${data.lat.toFixed(6)} ${names[1]}: ${data.lng.toFixed(6)}${data.alt ? ` ${names[2]}: ${data.alt.toFixed(1)}[m]` : ""}`:"";
 	draw_latlng();
 }
 function leave() { const w = 350, h = 25;
@@ -112,9 +106,9 @@ function destroy(data) {
 	postMessage({ type: data.type, action: "done" });
 }
 ////--------------------------------------------------------------
-function draw_border() {
-	if (!accessories.borders || zoom < accessories.borders.minZoom || accessories.borders.maxZoom < zoom) return;
-	accessories.borders.jsons.forEach(([json, prop]) => {
+function draw_border() { const q = accessories.borders;
+	if (!q || zoom < q.minZoom || q.maxZoom < zoom) return;
+	q.jsons.forEach(([json, prop]) => {
 		layer.save();
 		layer.beginPath(); path(json);
 		layer.strokeStyle = prop.stroke;
@@ -124,18 +118,18 @@ function draw_border() {
 		layer.restore();
 	});
 }
-function draw_latlng() { leave()
-	if (!accessories.latlng || zoom < accessories.latlng.minZoom||!accessories.latlng.string) return;
+function draw_latlng() { const q = accessories.latlng; leave()
+	if (!q || zoom < q.minZoom||!q.string) return;
 	layer.save();
-	layer.font = "12px Verdana"; layer.textBaseline = "middle"; layer.fillStyle = accessories.latlng.color;
+	layer.font = "12px Verdana"; layer.textBaseline = "middle"; layer.fillStyle = q.color;
 	layer.textAlign = isNarrow ? "center" : "left";
-	layer.fillText(accessories.latlng.string, isNarrow ? width / 2 : 10, isNarrow ? 10 : height - 10);
+	layer.fillText(q.string, isNarrow ? width / 2 : 10, isNarrow ? 10 : height - 10);
 	layer.restore();
 }
-function draw_scale() {
-    if (!accessories.scale || zoom < accessories.scale.minZoom) return;
+function draw_scale() { const q = accessories.scale;
+    if (!q || zoom < q.minZoom) return;
     const R = 6372000 * 2, M = width / 2, H = height - 30 - (isNarrow ? 20 : 0);
-    const useImperial = accessories.scale.unit === "imperial";
+    const useImperial = q.unit === "imperial";
     const [n, v, unit] = (function () {
         const d256m = (R * PI) / 2 ** zoom; // 256pxあたりのメートル
         if (useImperial) {
@@ -159,7 +153,7 @@ function draw_scale() {
     const str = `${comma(v.toFixed(decimal))}${unit} (z=${zoom.toFixed(2)})`;
     layer.save();
     layer.font = "12px Verdana"; layer.textBaseline = "bottom"; layer.textAlign = "center";
-    layer.strokeStyle = layer.fillStyle = accessories.scale.color;
+    layer.strokeStyle = layer.fillStyle = q.color;
     layer.beginPath();
     layer.moveTo(M - n / 2, H + 20); layer.lineTo(M + n / 2, H + 20);
     layer.lineWidth = 3; layer.stroke();
@@ -170,34 +164,35 @@ function draw_scale() {
     layer.fillText(str, M, H + 15);
     layer.restore();
 }
-function draw_credit() {
-	if (!accessories.credit || zoom < accessories.credit.minZoom) return;
+function draw_credit() { const q = accessories.credit;
+	if (!q || zoom < q.minZoom || !attribution) return;
 	layer.save();
-	layer.font = "12px Verdana"; layer.textBaseline = "middle"; layer.fillStyle = accessories.credit.color;
+	layer.font = "12px Verdana"; layer.textBaseline = "middle"; layer.fillStyle = q.color;
 	layer.textAlign = isNarrow ? "center" : "right";
 	layer.fillText(attribution, isNarrow ? width / 2 : width - 10, height - 10);
 	layer.restore();
 }
-function draw_globe() {
-	if (!accessories.globe || zoom > accessories.globe.maxZoom || zoom < noCircle) return;
-	const size = accessories.globe.size || 125, gap = isNarrow ? 25:0;
-	const x0 = accessories.globe.left? accessories.globe.left: width - size - (accessories.globe.bottom||20);
-	const y0 = accessories.globe.top? accessories.globe.top + gap: height - size - (accessories.globe.bottom||30) - gap;
+function draw_globe() { const q = accessories.globe;
+	if (!q || zoom > q.maxZoom || zoom < noCircle) return;
+	const size = q.size || 125, gap = isNarrow ? 25:0;
+	const x0 = q.left? q.left: width - size - (q.bottom||20);
+	const y0 = q.top? q.top + gap: height - size - (q.bottom||30) - gap;
 	const project = geoOrthographic().fitExtent([[x0, y0], [x0+size, y0+size]], sphere).precision(0.1);
 	const r = proj.rotate(); project.rotate([r[0], r[1], 0]);
 	const path = geoPath(project, layer);
 	const bounds = [[0, 0], [width, 0], [width, height], [0, height]].map(proj.invert).map(project);
 	layer.save();
 	layer.clearRect(x0, y0, x0+size, y0+size);
-	layer.beginPath(); path(sphere); layer.fillStyle = accessories.globe.sea; layer.fill();
-	layer.beginPath(); path(land110); layer.fillStyle = accessories.globe.land; layer.fill();
+	layer.beginPath(); path(sphere); layer.fillStyle = q.sea; layer.fill();
+	layer.beginPath(); path(q.json); layer.fillStyle = q.land; layer.fill();
 	layer.beginPath(); path(graticule); layer.strokeStyle = "rgba(255,255,255,0.5)"; layer.lineWidth = 1; layer.stroke();
 	layer.beginPath(); path(sphere); layer.strokeStyle = "rgba(255,255,255,0.5)"; layer.lineWidth = 2; layer.stroke();
 	layer.beginPath(); bounds.forEach((t, i) => layer[i ? "lineTo" : "moveTo"](t[0], t[1])); layer.closePath();
-	layer.strokeStyle = accessories.globe.line; layer.lineWidth = 1.5; layer.stroke();
+	layer.strokeStyle = q.line; layer.lineWidth = 1.5; layer.stroke();
 	layer.restore();
 }
-function draw_stars() { if (!accessories.stars || zoom > accessories.stars.maxZoom) return;
+function draw_stars() { const q = accessories.stars;
+	if (!q || zoom > q.maxZoom) return;
 	const dt = new Date();
 	const cx = width / 2, cy = height / 2, er = proj.scale(), er2 = er * er;
 	const sr = hypot(width, height) * (0.4 + zoom * 0.3);
@@ -205,7 +200,7 @@ function draw_stars() { if (!accessories.stars || zoom > accessories.stars.maxZo
 	const skyRot = (getSidereal(dt) - r[0]) * rad;
 	const sφ = sin(r[1] * rad), cφ = cos(r[1] * rad), sγ = sin(r[2] * rad), cγ = cos(r[2] * rad);
 	layer.save();
-	for (let s of stars) {
+	for (let s of q.data) {
 		const l = s.x - skyRot, cl = cos(l), sl = -sin(l);
 		const cp = cos(s.y), sp = sin(s.y);
 		const x = cp * sl, y = cφ * sp - sφ * cp * cl, z = sφ * sp + cφ * cp * cl; if (z < 0) continue;
@@ -217,7 +212,8 @@ function draw_stars() { if (!accessories.stars || zoom > accessories.stars.maxZo
 	}
 	layer.restore();
 }
-function draw_night() { if (!accessories.night || zoom > accessories.night.maxZoom) return;
+function draw_night() { const q = accessories.night;
+	if (!q || zoom > q.maxZoom) return;
 	const dt = new Date();
 	const cx = width / 2, cy = height / 2, er = proj.scale(), er2 = er * er;	
 	const halo = layer.createRadialGradient(cx, cy, er, cx, cy, er + 15);
@@ -228,7 +224,7 @@ function draw_night() { if (!accessories.night || zoom > accessories.night.maxZo
 	layer.fillStyle = halo;
 	layer.beginPath(); layer.arc(cx, cy, er + 15, 0, PI * 2); layer.fill();
 	layer.beginPath(); layer.arc(cx, cy, er, 0, PI * 2); layer.clip();
-	layer.beginPath(); path(accessories.night.json); layer.fillStyle = "rgba(0, 5, 20, 0.5)"; layer.fill();
+	layer.beginPath(); path(q.json); layer.fillStyle = "rgba(0, 5, 20, 0.5)"; layer.fill();
 	layer.restore();
 	layer.textAlign = "center"; layer.textBaseline = "middle"; layer.fillStyle = "#fff";
 	layer.font = `${32 * (2 - zoom) + 16}px Verdana`;
