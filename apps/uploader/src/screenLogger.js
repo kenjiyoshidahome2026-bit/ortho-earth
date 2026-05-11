32ac41726948f420c6d0c946da405c522479ae8e
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-05-11 14:36:01 +0900

    gishub

--- apps/uploader/src/screenLogger.js
+++ apps/uploader/src/screenLogger.js
@@ -33,17 +33,21 @@ export class screenLogger {
 		a.forEach(t=>p.append("span").html(toS(t)));
 	}
 	progress(name, type, total) {
-		if (type === "start") {
-			this.bars[name] = [this.target.append("p").classed("progress", true).text("⏳ " + name), performance.now(), 0];
+		if (type === "start") { if (this.bars[name]) return;
+			const bar = this.target.append("p").classed("progress", true).text("⏳ " + name)
+			this.bars[name] = [bar, performance.now(), 0];
 		} else if (type === "end" && this.bars[name]) {
 			const bar = this.bars[name][0], start = this.bars[name][1];
 			const time = ((performance.now() - start)/1000).toFixed(3);
-			bar.text(`⏳ ${name}: ${comma(total)} bytes / ${comma(time)}sec (${comma((total/time).toFixed(2))} bytes/sec)`);
+			const speed = comma((total / time / 1024 / 1024).toFixed(2));
+			bar.text(`⏳ ${name}: ${comma(total)} bytes / ${comma(time)}sec (${speed} Mbytes/sec)`);
+			this.bars[name] = null;
 			delete this.bars[name];
+		//	console.log(this.bars);
 		} else if (isNumber(type) && this.bars[name]) {
 			const bar = this.bars[name][0], count = this.bars[name][2] = this.bars[name][2]+1;
 			const pct = Math.round((type / total) * 100), n = Math.floor(pct / 5);
-			const p = `<span class='done'>${"▓".repeat(n)}</span><span class='rest'>${"░".repeat(20 - n)}</span>`;
+			const p = `<span class='done'>${"█".repeat(n)}</span><span class='rest'>${"█".repeat(20 - n)}</span>`;
 			const d = this.dots[count % this.dots.length];
 			bar.html(`⏳ ${name}: ${d}[${p}] ${pct}% (${comma(type)}/${comma(total)})`);
 		}
