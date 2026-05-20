f083bd1faa79f568dfce05f2cc348904aa3da937
Kenji <kenji.yoshida.home.2026@gmail.com>
2026-05-20 09:38:41 +0900

    vite

--- packages/ortho-map/vite.config.js
+++ packages/ortho-map/vite.config.js
@@ -1,44 +1,10 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-
 export default defineConfig({
-    resolve: {
-        alias: {
-            'common': resolve(__dirname, '../common/src'),
-            'native-bucket': resolve(__dirname, '../native-bucket/src'),
-            'geopbf': resolve(__dirname, '../geopbf/src'),
-            'altpbf': resolve(__dirname, '../altpbf/src'),
-        }
-    },
     worker: {
-        format: 'es',
-        // Worker内で使われるライブラリをすべて一つのファイルにまとめる設定
-        rollupOptions: {
-            output: {
-                inlineDynamicImports: true
-            }
-        }
+        format: 'iife',
     },
     build: {
-        minify: false,
+        minify: true,
         sourcemap: true,
-        lib: {
-            entry: './src/index.js',
-            fileName: 'index',
-            formats: ['es']
-        },
-        rollupOptions: {
-            // ライブラリモードで外部依存関係を正しく処理するため
-            external: ['canvas', 'd3'], 
-            output: {
-                globals: {
-                    canvas: 'Canvas',
-                    d3: 'd3'
-                }
-            }
-        }
-    },
-	optimizeDeps: {
-		exclude: ['canvas']// 依存関係の事前ビルドからも除外
-	}
+    }
 });
