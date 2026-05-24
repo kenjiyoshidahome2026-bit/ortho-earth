2198b1ccfaac73a55049e466c80c4a35fabc0d61
Kenji Yoshida <kenji.yoshida.home.2026@gmail.com>
2026-05-25 05:40:04 +0900

    json.js

--- packages/geopbf/src/decoder/json.js
+++ packages/geopbf/src/decoder/json.js
@@ -1,30 +1,24 @@
 import { GeoPBF } from "../pbf-base.js";
-import { geojsonReader } from "common";
+import { geojsonReader, isObject } from "common";
 const threshold = 50 * 1024 * 1024;
 onmessage = async (e) => {
     const file = e.data.file;
+    const pbf = new GeoPBF(e.data);
     if (file.size < threshold) {
-        const json = JSON.parse(await file.text());
-        const pbf = new GeoPBF(e.data);
-        await pbf.set(json);
-        const res = pbf.arrayBuffer;
-        postMessage({ type: "jsondec", data: res }, [res]);
+        await pbf.set(JSON.parse(await file.text()));
     } else {
         const keySet = new Set();
-        const getPropertyKeys = f => { if (!f.properties) return;
+        const getPropertyKeys = f => { if (!isObject(f.properties)) return;
             for (const k in f.properties) { keySet.add(k);
-                const v = f.properties[k];
-                if (v && typeof v === 'object' && !Array.isArray(v)) {
-                    for (const sk in v) keySet.add(`${k}.${sk}`);
-                }
+                const v = f.properties[k]; if (!isObject(v)) continue;
+                for (const sk in v) keySet.add(`${k}.${sk}`);
             }
         };
         await geojsonReader(file, getPropertyKeys, false);
-        const pbf = new GeoPBF(e.data);
         pbf.setHead(Array.from(keySet).sort());
         pbf.setBody(() => geojsonReader(file, f => pbf.setFeature(f), true));
-        pbf.close();
-        const res = pbf.arrayBuffer;
-        postMessage({ type: "jsondec", data: res }, [res]);
     }
+    pbf.close();
+    const res = pbf.arrayBuffer;
+    postMessage({ type: "jsondec", data: res }, [res]);
 };
